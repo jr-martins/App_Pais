d68e0aa334aae8e037a90574aa2ce385bea4bb6b
Your Name <jrmartins1000@gmail.com>
2019-09-30 19:21:48 -0300

    Paises

--- app/src/main/java/br/com/teste/app_paises/MainActivity.java
+++ app/src/main/java/br/com/teste/app_paises/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Lista de Países");
         List<String> paises = new ArrayList<>(
                 Arrays.asList("Brasil", "Argentina", "Chile"));
 
